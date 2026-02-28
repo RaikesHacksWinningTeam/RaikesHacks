740de8cd72787b8a439ef52042d19ca3d29e03d8
Caleb Nierman <calebjnierman@proton.me>
2026-02-28 15:40:50 -0600

    Color picker and org deleter

--- api/static/js/api.js
+++ api/static/js/api.js
@@ -49,3 +49,17 @@ export async function joinOrgAPI(invite_code) {
 export async function fetchOrgMembersAPI(orgId) {
     return fetch(`/api/orgs/${orgId}/members`);
 }
+
+export async function updateOrgAPI(orgId, data) {
+    return fetch(`/api/orgs/${orgId}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    });
+}
+
+export async function deleteOrgAPI(orgId) {
+    return fetch(`/api/orgs/${orgId}`, {
+        method: 'DELETE'
+    });
+}
